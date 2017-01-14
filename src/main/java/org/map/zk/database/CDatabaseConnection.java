d79a932e294d1ecd1c4ddb9bbb5cd3fda211ba6f
German <German@192.168.1.107>
2017-01-14 17:32:51 -0400

    Database Config

---
+++ src/main/java/org/map/zk/database/CDatabaseConnection.java
@@ -0,0 +1,14 @@
+package org.map.zk.database;
+
+import java.io.Serializable;
+import java.sql.Connection;
+
+public class CDatabaseConnection implements Serializable {
+
+    private static final long serialVersionUID = 2311816988364334105L;
+    
+    protected Connection dbConnection;
+    
+    
+    
+}
